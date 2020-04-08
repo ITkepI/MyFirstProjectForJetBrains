dceffc6296e4326370d76f4297901b0bc82fb531
ITkepI <31821791+ITkepI@users.noreply.github.com>
2020-04-08 17:23:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace alternative_Подсказки_для_неявных_операций
+namespace JetBrainsProject
 {
     internal class RPN
     {
